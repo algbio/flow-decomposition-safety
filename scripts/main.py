0ab94c9ad4eb5543a284a569efb241821ef9e124
millakortelainen <milla.kortelainen@helsinki.fi>
2021-05-25 17:04:28 +0300

    use network X as a representation of graph

--- scripts/main.py
+++ scripts/main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import networkx as nx
+from networkx.algorithms.flow import edmonds_karp
+from networkx.algorithms.flow import shortest_augmenting_path
 
 def main():
     file = 'data/dag_graph.gfa'
@@ -6,12 +9,14 @@ def main():
     # print(graph)
     paths = read_paths(file)
     # print(paths)
-    all_max_safe_paths(paths)
+    #all_max_safe_paths(paths)
+    ff = nx.maximum_flow(graph, 0, 7)
+    print(ff)
 
 
 def read_graph(filename):
     # save graph as a adjacency list
-    graph = {}
+    graph = nx.Graph()
     with open(filename, 'r') as f:
         for line in f:
             # read line
@@ -21,19 +26,13 @@ def read_graph(filename):
             else:
                 read = (line.rstrip()).split(' ')
 
-            # add vertices to graph
-            if line[0] == 'S':
-                v_index = int(read[1])
-                graph[v_index] = []
-
             # add edge to the adjecency list
             if line[0] == 'L':
                 v_from = int(read[1])
                 v_to = int(read[3])
                 weight = int((read[5])[0:-1])
-                graph[v_from].append((v_to, weight))
+                graph.add_edge(v_from, v_to, capacity=weight)
     return graph
-
 
 def read_paths(filename):
     paths = []
@@ -42,25 +41,6 @@ def read_paths(filename):
             if line[0] == 'P':
                 paths.append(line.rstrip())
     return paths
-
-def all_max_safe_paths(paths):
-    max_path = ''
-    max_flow = 0
-    for p in paths:
-        parts = p.split(' ')
-        weights = parts[3].split(',')
-        weights2 = [int(x[0:-1]) for x in weights]
-        flow = 9999
-        # flow is a minimum of flows of the graph
-        for w in weights2:
-            if flow > w:
-                flow = w
-        if flow > max_flow:
-            max_flow = flow
-            max_path = p
-    print(max_path)
-    
-
 
 if __name__ == '__main__':
     main()
