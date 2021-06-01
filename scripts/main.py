290a8bfe1bd125eeb31f15cf9e573458e8172ba7
millakortelainen <milla.kortelainen@helsinki.fi>
2021-06-01 13:54:29 +0300

    .graph file processing and drawing of graphs

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 import networkx as nx
+import pygraphviz as pgv
 
 
 class Graph:
-    def __init__(self, graph):
+    def __init__(self, graph, n=0, s=0, t=0):
         self.graph = graph
+        self.n = n
+        self.s = s
+        self.t = t
+        if n != 0:
+            self.s = 0
+            self.t = n-1
 
     def excess_flow(self, path):
         flow_sum = 0
@@ -39,51 +46,157 @@ class Graph:
                     f += (f_in - self.graph.edges[first]['capacity'])
         return max_safe_paths
 
+    def flow_decomposition(self):
+        stack = [self.s]
+        min_flow = 999999
+        path = []
+        paths = []
+        copy_of_graph = self.graph.copy()
+
+        while(len(stack) > 0):
+            v = stack.pop()
+
+            if v == self.t:
+                paths.append(path)
+                for e in path:
+                    copy_of_graph.edges[e]['capacity'] -= min_flow
+
+                path = []
+                min_flow = 9999
+                stack = [self.s]
+
+                cap = 0
+                for e in copy_of_graph.out_edges(self.s):
+                    cap += copy_of_graph.edges[e]['capacity']
+                if cap == 0:
+                    return paths
+            else:
+                max_route = 0
+                max_flow = 0
+                for e in copy_of_graph.out_edges(v):
+                    if copy_of_graph.edges[e]['capacity'] > max_flow:
+                        max_flow = copy_of_graph.edges[e]['capacity']
+                        max_route = e[1]
+                stack.append(max_route)
+                path.append((v, max_route))
+
+                if copy_of_graph.edges[v, max_route]['capacity'] < min_flow:
+                    min_flow = copy_of_graph.edges[v, max_route]['capacity']
+
+    def print(self):
+        for e in self.graph.edges:
+            print(e, ' capacity: ', self.graph.edges[e]['capacity'])
+
 
 def main():
-    file = 'data/graph.gfa'
-    graph = read_graph(file)
-    g = Graph(graph)
-    composed_paths = flow_decomposition(graph.copy(), 0, 13)
-    print("composed paths")
-    for p in composed_paths:
-        print(p)
-    print("*****")
-    max_safe_paths = g.maximal_safe_paths(composed_paths)
-    print("maximum safe paths:")
-    for p in max_safe_paths:
-        print(p)
+    file = 'data/1.graph'
+    graphs = read_graph(file, 6)
+    i = 0
+    for g in graphs:
+        print('graph')
+        g.print()
+        A = nx.nx_agraph.to_agraph(g.graph)
+        A.layout()
+        A.draw(f'file{i}.png')
+        print('decomposition')
+        dec = g.flow_decomposition()
+        for d in dec:
+            print(d)
+        i += 1
+        print('max safe paths')
+        max_safe = g.maximal_safe_paths(dec)
+        for p in max_safe:
+            print(p)
+        print('***************')
+        print('example:')
+        file = 'data/graph.gfa'
+        graph=read_graph(file)
+        g = Graph(graph,0,0,13)
+        A = nx.nx_agraph.to_agraph(g.graph)
+        A.layout()
+        A.draw(f'example.png')
+        composed_paths = g.flow_decomposition()
+        print("composed paths")
+        for p in composed_paths:
+            print(p)
+        max_safe_paths = g.maximal_safe_paths(composed_paths)
+        print("maximum safe paths:")
+        for p in max_safe_paths:
+            print(p)
+    
 
 
-def read_graph(filename):
-    graph = nx.DiGraph()
-    with open(filename, 'r') as f:
-        for line in f:
-            # read line
-            read = ''
-            if line.rstrip().count('\t') > 0:
-                read = (line.rstrip()).split('\t')
-            else:
-                read = (line.rstrip()).split(' ')
+def read_graph(filename, n=0):
 
-            # add edge
-            if line[0] == 'L':
-                v_from = int(read[1])
-                v_to = int(read[3])
-                weight = int((read[5])[0:-1])
-                graph.add_edge(v_from, v_to, capacity=weight)
-                if 'flow_out' not in graph.nodes[v_from]:
-                    graph.nodes[v_from]['flow_out'] = 0
-                if 'flow_out' not in graph.nodes[v_to]:
-                    graph.nodes[v_to]['flow_out'] = 0
-                if 'flow_in' not in graph.nodes[v_from]:
-                    graph.nodes[v_from]['flow_in'] = 0
-                if 'flow_in' not in graph.nodes[v_to]:
-                    graph.nodes[v_to]['flow_in'] = 0
+    str = filename.split('.')
 
-                graph.nodes[v_to]['flow_in'] += weight
-                graph.nodes[v_from]['flow_out'] += weight
-    return graph
+    if str[-1] == 'graph':
+        graphs = []
+        i = 0
+        g = nx.DiGraph()
+        graph_size = 0
+        with open(filename, 'r') as f:
+            for line in f:
+                if i >= n:
+                    break
+                read_line = line.rstrip().split(' ')
+                if read_line[0] == '#':
+                    if graph_size != 0:
+                        graphs.append(Graph(g, graph_size))
+                    i += 1
+                    g = nx.DiGraph()
+                    graph_size = 0
+                elif len(read_line) == 1:
+                    graph_size = int(read_line[0])
+                else:
+                    g.add_edge(int(read_line[0]), int(
+                        read_line[1]), capacity=float(read_line[2]))
+                    init_node(g, int(read_line[0]), int(
+                        read_line[1]), float(read_line[2]))
+        return graphs
+    elif str[-1] == 'gfa':
+        graph = nx.DiGraph()
+
+        with open(filename, 'r') as f:
+            for line in f:
+                # read line
+                read = ''
+                if line.rstrip().count('\t') > 0:
+                    read = (line.rstrip()).split('\t')
+                else:
+                    read = (line.rstrip()).split(' ')
+
+                # add edge
+                if line[0] == 'L':
+                    v_from = int(read[1])
+                    v_to = int(read[3])
+                    weight = int((read[5])[0:-1])
+                    graph.add_edge(v_from, v_to, capacity=weight)
+                    if 'flow_out' not in graph.nodes[v_from]:
+                        graph.nodes[v_from]['flow_out'] = 0
+                    if 'flow_out' not in graph.nodes[v_to]:
+                        graph.nodes[v_to]['flow_out'] = 0
+                    if 'flow_in' not in graph.nodes[v_from]:
+                        graph.nodes[v_from]['flow_in'] = 0
+                    if 'flow_in' not in graph.nodes[v_to]:
+                        graph.nodes[v_to]['flow_in'] = 0
+
+                    graph.nodes[v_to]['flow_in'] += weight
+                    graph.nodes[v_from]['flow_out'] += weight
+            return graph
+
+
+def init_node(graph, v_from, v_to, weight):
+    if 'flow_out' not in graph.nodes[v_from]:
+        graph.nodes[v_from]['flow_out'] = 0
+    if 'flow_out' not in graph.nodes[v_to]:
+        graph.nodes[v_to]['flow_out'] = 0
+    if 'flow_in' not in graph.nodes[v_from]:
+        graph.nodes[v_from]['flow_in'] = 0
+    if 'flow_in' not in graph.nodes[v_to]:
+        graph.nodes[v_to]['flow_in'] = 0
+    graph.nodes[v_to]['flow_in'] += weight
+    graph.nodes[v_from]['flow_out'] += weight
 
 #not used
 
@@ -95,43 +208,6 @@ def read_paths(filename):
             if line[0] == 'P':
                 paths.append(line.rstrip())
     return paths
-
-
-def flow_decomposition(graph, s, t):
-    stack = [s]
-    min_flow = 9999
-    path = []
-    paths = []
-
-    while(len(stack) > 0):
-        v = stack.pop()
-
-        if v == t:
-            paths.append(path)
-            for e in path:
-                graph.edges[e]['capacity'] -= min_flow
-
-            path = []
-            min_flow = 9999
-            stack = [s]
-
-            cap = 0
-            for e in graph.out_edges(s):
-                cap += graph.edges[e]['capacity']
-            if cap == 0:
-                return paths
-        else:
-            max_route = 0
-            max_flow = 0
-            for e in graph.out_edges(v):
-                if graph.edges[e]['capacity'] > max_flow:
-                    max_flow = graph.edges[e]['capacity']
-                    max_route = e[1]
-            stack.append(max_route)
-            path.append((v, max_route))
-
-            if graph.edges[v, max_route]['capacity'] < min_flow:
-                min_flow = graph.edges[v, max_route]['capacity']
 
 
 if __name__ == '__main__':
