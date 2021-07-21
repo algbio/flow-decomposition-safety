4a31ef25d12b293a0ecaea1391159e15ae3bb637
Milla <milla.kortelainen@helsinki.fi>
2021-07-21 14:59:09 +0300

    simple tests for Graph

--- src/tests/test_graph.py
+++ src/tests/test_graph.py
@@ -6,33 +6,39 @@ import networkx as nx
 class GraphTest(unittest.TestCase):
 
     def setUp(self):
-        self.nx_graph = nx.DiGraph()
+        self.nx_simple_graph = nx.DiGraph()
 
-        self.nx_graph.add_edge(1, 2, capacity=19)
-        self.nx_graph.nodes[1]['flow_out'] = 19
-        self.nx_graph.nodes[2]['flow_in'] = 19
-        self.nx_graph.add_edge(2, 3, capacity=19)
-        self.nx_graph.nodes[2]['flow_out'] = 19
-        self.nx_graph.nodes[3]['flow_in'] = 19
-        self.nx_graph.add_edge(3, 4, capacity=19)
-        self.nx_graph.nodes[3]['flow_out'] = 19
-        self.nx_graph.nodes[4]['flow_in'] = 19
-        self.nx_graph.add_edge(0, 1, capacity=19)
-        self.nx_graph.nodes[0]['flow_out'] = 19
-        self.nx_graph.nodes[1]['flow_in'] = 19
+        self.nx_simple_graph.add_edge(1, 2, capacity=19)
+        self.nx_simple_graph.nodes[1]['flow_out'] = 19
+        self.nx_simple_graph.nodes[2]['flow_in'] = 19
+        self.nx_simple_graph.add_edge(2, 3, capacity=19)
+        self.nx_simple_graph.nodes[2]['flow_out'] = 19
+        self.nx_simple_graph.nodes[3]['flow_in'] = 19
+        self.nx_simple_graph.add_edge(3, 4, capacity=19)
+        self.nx_simple_graph.nodes[3]['flow_out'] = 19
+        self.nx_simple_graph.nodes[4]['flow_in'] = 19
+        self.nx_simple_graph.add_edge(0, 1, capacity=19)
+        self.nx_simple_graph.nodes[0]['flow_out'] = 19
+        self.nx_simple_graph.nodes[1]['flow_in'] = 19
 
-        self.graph = Graph(graph=self.nx_graph, s=0, t=4)
+        self.simple_graph = Graph(graph=self.nx_simple_graph, s=0, t=4)
     
     def test_graph_initial_state(self):
-        self.assertEqual(self.nx_graph, self.graph.graph)
-        self.assertEqual(0, self.graph.s)
-        self.assertEqual(4, self.graph.t)
-        self.assertEqual([], self.graph.flow_decomposition_paths)
-        self.assertEqual([], self.graph.max_safe_paths)
-        self.assertEqual({'decomposition': 0, 'safety': 0}, self.graph.times)
+        self.assertEqual(self.nx_simple_graph, self.simple_graph.graph)
+        self.assertEqual(0, self.simple_graph.s)
+        self.assertEqual(4, self.simple_graph.t)
+        self.assertEqual([], self.simple_graph.flow_decomposition_paths)
+        self.assertEqual([], self.simple_graph.max_safe_paths)
+        self.assertEqual({'decomposition': 0, 'safety': 0}, self.simple_graph.times)
 
 
     def test_flow_decomposition(self):
-        pass 
+        self.assertEqual([[(0, 1), (1, 2), (2, 3), (3, 4)]], self.simple_graph.flow_decomposition())
+        self.assertEqual([[(0, 1), (1, 2), (2, 3), (3, 4)]], self.simple_graph.flow_decomposition_paths)
+    
+    def test_maximum_safe_path(self):
+        self.assertEqual([[(0, 1), (1, 2), (2, 3), (3, 4)]], self.simple_graph.maximal_safe_paths())
+        self.assertEqual([[(0, 1), (1, 2), (2, 3), (3, 4)]], self.simple_graph.max_safe_paths)
+
 if __name__ == '__main__':
     unittest.main()
