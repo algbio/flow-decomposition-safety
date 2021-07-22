3568e67c895ea02fc6320c3876b7020d07b8ce3f
Milla <milla.kortelainen@helsinki.fi>
2021-07-22 15:47:17 +0300

    documentation and tests

--- src/scripts/io_helper.py
+++ src/scripts/io_helper.py
@@ -1,24 +1,35 @@
+'''
+io_helper reads and graphs to files
+'''
 from src.scripts.graph import Graph
 import networkx as nx
 
 
 def write_file(str, output):
+    '''
+    write_file(str, output)
+    Writes given string end of the given output file.
+    '''
     f = open(output, 'a')
     f.write(f'{str}')
 
 
 def read_gfa_file(filename):
+    '''
+    read_gfa_file(filename) -> list of Graph
+    Reads given file and return graphs described in .gfa file as a list of Graph objects.
+    '''
     graphs = []
     graph = None
     with open(filename, 'r') as f:
         for i, line in enumerate(f):
-            # read line
-            read = (line.rstrip()).split('\t')
-            # add edge
+            read = (line.rstrip()).split()
+            # Title line in gfa file, begins a new graph
             if line[0] == 'H':
                 if i != 0:
                     graphs.append(Graph(graph, 0, len(graph.nodes)-1))
                 graph = nx.DiGraph()
+            # Edge line in gfa file
             if line[0] == 'L':
                 v_from = int(read[1])
                 v_to = int(read[3])
@@ -31,6 +42,7 @@ def read_gfa_file(filename):
 
 # fix this to not to be dependent on path of the file
 def read_file(filename, type=None):
+    
     filename_parts = filename.split('/')
 
     graphs = []
@@ -70,12 +82,12 @@ def read_file(filename, type=None):
 
 
 def init_node(nodes, v_from, v_to, weight):
+    '''
+    init_nodes(nodes, v_from, v_to, weight)
+    Initializes flow of the node.
+    '''
     if 'flow_out' not in nodes[v_from]:
         nodes[v_from]['flow_out'] = 0
-    if 'flow_out' not in nodes[v_to]:
-        nodes[v_to]['flow_out'] = 0
-    if 'flow_in' not in nodes[v_from]:
-        nodes[v_from]['flow_in'] = 0
     if 'flow_in' not in nodes[v_to]:
         nodes[v_to]['flow_in'] = 0
     nodes[v_to]['flow_in'] += weight
--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,41 +1,37 @@
+'''
+Main class for using safety algorithm
+'''
 #!/usr/bin/python3
 import argparse
-import os
 from src.scripts import io_helper
 from timeit import default_timer as timer
 
 
-def main():
-    if os.path.isfile('data/output.txt'):
-        os.remove('data/output.txt')
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--graph_file", help="path to file")
-    parser.add_argument("-o", "--output_file", help="path to file")
-    args = parser.parse_args()
-
-    file = args.graph_file
-    graphs = io_helper.read_gfa_file(file)
+def main(input_file, output_file, timer = False):
+    graphs = io_helper.read_gfa_file(input_file)
     i = 0
     algotime = 0
     decompositiontime = 0
     safetytime = 0
     for g in graphs:
-        io_helper.write_file(f'# graph {i}', args.output_file)
-        start = timer()
-        max = g.maximal_safe_paths()
-        end = timer()
-        algotime += (end-start)
-        decompositiontime += g.get_decomposition_time()
-        safetytime += g.get_safety_time()
-        for m in max:
-            io_helper.write_file(path_to_string(m), args.output_file)
+        io_helper.write_file(f'# graph {i}\n', output_file)
+        if timer:
+            start = timer()
+        g.maximal_safety()
+        if timer:
+            end = timer()
+            algotime += (end-start)
+            decompositiontime += g.get_decomposition_time()
+            safetytime += g.get_safety_time()
+        io_helper.write_file(g.safe_paths_to_string(), output_file)
         i += 1
-    print('time used to algorithm')
-    print(algotime)
-    print('time used to decomposition algorithm')
-    print(decompositiontime)
-    print('time used to safety algorithm')
-    print(safetytime)
+    if timer:
+        print('time used to algorithm')
+        print(algotime)
+        print('time used to decomposition algorithm')
+        print(decompositiontime)
+        print('time used to safety algorithm')
+        print(safetytime)
 
 
 def path_to_string(path):
@@ -44,14 +40,24 @@ def path_to_string(path):
     for p in path:
         str += f'{p[0]} '
         if i == len(path)-1:
-            str += f'{p[1]}'
+            str += f'{p[1]}\n'
         i += 1
     return str
 
 
 if __name__ == '__main__':
-    main_start = timer()
-    main()
-    main_end = timer()
-    print('executing the whole code')
-    print(f'{main_end-main_start}s')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--graph_file", help="path to input file")
+    parser.add_argument("-o", "--output_file", help="path to output file")
+    parser.add_argument("-t", "--timer", help="measure how long the execution takes, default: False", default=False)
+    args = parser.parse_args()
+
+    file = args.graph_file
+    if args.timer:
+        main_start = timer()
+        main(args.graph_file, args.output_file)
+        main_end = timer()
+        print('executing the whole code')
+        print(f'{main_end-main_start}s')
+    else:
+        main(args.graph_file, args.output_file)
