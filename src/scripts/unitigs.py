ff6e547e83260eadcd0b6549776d11d2d880faeb
millakortelainen <milla.kortelainen@helsinki.fi>
2021-08-05 14:41:37 +0300

    minor fixes to unitigs

--- src/scripts/unitigs.py
+++ src/scripts/unitigs.py
@@ -7,7 +7,7 @@ from src.scripts.main import flow_decomposition, to_vertex_list
 from src.scripts import io_helper
 
 
-def main(input_file, mode):
+def main(input_file):
     graphs = io_helper.read_gfa_file(input_file)
     i = 0
     for g in graphs:
@@ -17,7 +17,7 @@ def main(input_file, mode):
         for path in decomposition_paths:
             list = get_unitigs(path, g)
             for x in list:
-                unitgs_set.add(x)
+                unitgs_set.add(tuple(x))
         print(unitgs_set)
         i += 1
 
@@ -37,22 +37,20 @@ def get_unitigs(path, graph):
                 # add last constructed unitig
                 if unitig:
                     unitig.append(vertex_path[last_added+1])
-                    unitigs.append(tuple(unitig))
+                    unitigs.append(unitig)
                 # begin new unitig
                 unitig = [vertex_path[i-1], v]
             last_added = i
     if unitig:
         unitig.append(vertex_path[last_added+1])
-        unitigs.append(tuple(unitig)) 
+        unitigs.append(unitig) 
     return unitigs
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--graph_file", help="path to input file")
-    parser.add_argument("-m", "--mode",
-                        help="Default mode outputs path file, mode 'indices' outputs file with flow decomposition and indices of safe paths", default=None)
     args = parser.parse_args()
 
     file = args.graph_file
-    main(args.graph_file, args.mode)
+    main(args.graph_file)
